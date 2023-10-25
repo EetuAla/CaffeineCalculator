0c3690609974e429fc5722941eea928cbbff9a0a
EetuAla <ezka.alanen@gmail.com>
2023-10-25 14:18:11 +0300

    First commit

---
+++ src/test/java/com/example/CaffeineCalculator/CaffeineCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaffeineCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeineCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
